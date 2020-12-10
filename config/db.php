696b5ba3d4323a16e3418bf98487d0c6283b6d99
julien willette <julien.willette@gmail.com>
2020-12-10 18:43:10 +0100

    deploiement

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file define BD access infos.
+ *
+ * PHP version 7
+ *
+ * @category  PHP
+ * @package   PHP_SimpleMVC
+ * @author    WCS <contact@wildcodeschool.fr>
+ * @license  --
+ * @link     https://github.com/WildCodeSchool/simple-mvc
+ *
+ */
+
+define('APP_DB_USER', 'muxu_julien_9421');
+define('APP_DB_PWD', 'HWh6QYL3TaU1-6666et0');
+define('APP_DB_HOST', '618ce162-2fb3-4e0e-9b2a-ac1a85f4a44c.muxu-julien-9421.mysql.dbs.scalingo.com:34252/muxu_julien_9421?useSSL=true&verifyServerCertificate=false');
+define('APP_DB_NAME', 'muxu_julien_9421');
